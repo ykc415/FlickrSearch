33fdccc9c3833aef7d8573a6e46a380a4b4aa06b
ykc415 <pica415@gmail.com>
2019-03-02 19:11:09 +0900

    Refactor: inline 최적화 코드 추가

--- app/src/main/java/com/example/flickrsearch/ui/base/MvRxEpoxyController.kt
+++ app/src/main/java/com/example/flickrsearch/ui/base/MvRxEpoxyController.kt
@@ -22,8 +22,8 @@ open class MvRxEpoxyController(
 /**
  * Create a [MvRxEpoxyController] that builds models with the given callback.
  */
-fun BaseFragment.simpleController(
-    buildModels: EpoxyController.() -> Unit
+inline fun BaseFragment.simpleController(
+    crossinline buildModels: EpoxyController.() -> Unit
 ) = MvRxEpoxyController {
     // Models are built asynchronously, so it is possible that this is called after the fragment
     // is detached under certain race conditions.
@@ -35,9 +35,9 @@ fun BaseFragment.simpleController(
  * Create a [MvRxEpoxyController] that builds models with the given callback.
  * When models are built the current state of the viewmodel will be provided.
  */
-fun <S : MvRxState, A : MvRxViewModel<S>> BaseFragment.simpleController(
+inline fun <S : MvRxState, A : MvRxViewModel<S>> BaseFragment.simpleController(
     viewModel: A,
-    buildModels: EpoxyController.(state: S) -> Unit
+    crossinline buildModels: EpoxyController.(state: S) -> Unit
 ) = MvRxEpoxyController {
     if (view == null || isRemoving) return@MvRxEpoxyController
     withState(viewModel) { state ->
@@ -49,10 +49,10 @@ fun <S : MvRxState, A : MvRxViewModel<S>> BaseFragment.simpleController(
  * Create a [MvRxEpoxyController] that builds models with the given callback.
  * When models are built the current state of the viewmodels will be provided.
  */
-fun <A : BaseMvRxViewModel<B>, B : MvRxState, C : BaseMvRxViewModel<D>, D : MvRxState> BaseFragment.simpleController(
+inline fun <A : BaseMvRxViewModel<B>, B : MvRxState, C : BaseMvRxViewModel<D>, D : MvRxState> BaseFragment.simpleController(
     viewModel1: A,
     viewModel2: C,
-    buildModels: EpoxyController.(state1: B, state2: D) -> Unit
+    crossinline buildModels: EpoxyController.(state1: B, state2: D) -> Unit
 ) = MvRxEpoxyController {
     if (view == null || isRemoving) return@MvRxEpoxyController
     withState(viewModel1, viewModel2) { state1, state2 ->
@@ -64,11 +64,11 @@ fun <A : BaseMvRxViewModel<B>, B : MvRxState, C : BaseMvRxViewModel<D>, D : MvRx
  * Create a [MvRxEpoxyController] that builds models with the given callback.
  * When models are built the current state of the viewmodels will be provided.
  */
-fun <A : BaseMvRxViewModel<B>, B : MvRxState, C : BaseMvRxViewModel<D>, D : MvRxState, E : BaseMvRxViewModel<F>, F : MvRxState> BaseFragment.simpleController(
+inline fun <A : BaseMvRxViewModel<B>, B : MvRxState, C : BaseMvRxViewModel<D>, D : MvRxState, E : BaseMvRxViewModel<F>, F : MvRxState> BaseFragment.simpleController(
     viewModel1: A,
     viewModel2: C,
     viewModel3: E,
-    buildModels: EpoxyController.(state1: B, state2: D, state3: F) -> Unit
+    crossinline buildModels: EpoxyController.(state1: B, state2: D, state3: F) -> Unit
 ) = MvRxEpoxyController {
     if (view == null || isRemoving) return@MvRxEpoxyController
     withState(viewModel1, viewModel2, viewModel3) { state1, state2, state3 ->
