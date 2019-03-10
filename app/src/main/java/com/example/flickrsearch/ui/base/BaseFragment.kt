96bf4088529fe834f1e97d3b641ae4e1663bf82b
ykc415 <pica415@gmail.com>
2019-03-11 00:45:46 +0900

    Fix: MvRx 프래그먼트 라이프사이클 이슈로 recyclerview 포지션 저장안되고 @PersistState 안먹히던 버그 해결
    참조 : https://github.com/airbnb/MvRx/issues/134

--- app/src/main/java/com/example/flickrsearch/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/flickrsearch/ui/base/BaseFragment.kt
@@ -1,15 +1,20 @@
 package com.example.flickrsearch.ui.base
 
 import android.os.Bundle
-import com.airbnb.mvrx.BaseMvRxFragment
+import androidx.fragment.app.Fragment
+import com.airbnb.mvrx.MvRxView
+import com.airbnb.mvrx.MvRxViewModelStore
 
-abstract class BaseFragment: BaseMvRxFragment() {
+abstract class BaseFragment: Fragment(), MvRxView {
 
     protected val epoxyController by lazy { epoxyController() }
 
+    override val mvrxViewModelStore by lazy { MvRxViewModelStore(viewModelStore) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        mvrxViewModelStore.restoreViewModels(this, savedInstanceState)
         epoxyController.onRestoreInstanceState(savedInstanceState)
+        super.onCreate(savedInstanceState)
     }
 
     /**
@@ -20,7 +25,14 @@ abstract class BaseFragment: BaseMvRxFragment() {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
+        mvrxViewModelStore.saveViewModels(outState)
         epoxyController.onSaveInstanceState(outState)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        invalidate()
     }
 
     override fun onDestroyView() {
